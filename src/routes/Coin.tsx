5a82dfb324302d708ab49a40f0c228959024ebb9
rladuddbs <rladuddbs123@blockodyssey.io>
2022-05-20 09:54:16 +0900

    setting

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Coin = () => {
+  return <div>코인</div>;
+};
+
+export default Coin;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Coins = () => {
+  return <div>코인들</div>;
+};
+
+export default Coins;
